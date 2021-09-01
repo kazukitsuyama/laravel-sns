56e0e77be184fe2f79f86f5d252e3d307c6ed7b8
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-09-02 02:50:52 +0900

    articleコントローラーcreate定義

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Article;
 
+use App\Http\Requests\ArticleRequest;
+
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -18,6 +20,15 @@ class ArticleController extends Controller
    public function create()
     {
         return view('articles.create');    
+    }
+
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->title = $request->title;
+        $article->body = $request->body;
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
     }
 
 }

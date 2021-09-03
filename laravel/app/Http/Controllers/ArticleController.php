84af3213238203bed2b80d1d46a6c4e530661789
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-09-04 02:54:20 +0900

    コントローラにdestroyアクション定義

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -41,4 +41,10 @@ class ArticleController extends Controller
         return redirect()->route('articles.index');
     }
 
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
+
 }

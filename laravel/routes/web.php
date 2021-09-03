a63a1accd872d22118fb4aae85ca2c35b9e59735
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-09-04 02:57:23 +0900

    記事詳細のルーティング編集

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Routing\Route;
+
 Auth::routes(); 
 Route::get('/', 'ArticleController@index')->name('articles.index');
-Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->only(['show']);

d5ac8be14c63d999506199420c5f70be523b017b
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-09-04 01:57:12 +0900

    コントローラにeditアクション定義

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -30,4 +30,9 @@ class ArticleController extends Controller
         return redirect()->route('articles.index');
     }
 
+    public function edit(Article $article)
+    {
+        return view('articles.edit', ['article' => $article]);
+    }
+
 }
